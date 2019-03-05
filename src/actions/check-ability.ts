8fa926c212545ef25459346ba3ec34cffb9bf193
Code Hz <CodeHz@outlook.com>
2019-03-05 10:28:14 +0800

    split actions/check-ability

---
+++ src/actions/check-ability.ts
@@ -0,0 +1,27 @@
+import { MySystem } from "../system";
+import { db, SELECT_GUARD } from "../database";
+
+export function register(self: IStoneServerSystem<MySystem> & MySystem) {
+  self.checkAbility(player => {
+    const info = this.actorInfo(player);
+    if (info.permission > 0) return true;
+    const ret = db.query(SELECT_GUARD, {
+      $x: info.pos[0],
+      $z: info.pos[2]
+    });
+    const filtered = Array.from(ret).filter(({ owner }) => owner != info.uuid);
+    if (filtered.length) {
+      this.openModalForm(
+        player,
+        JSON.stringify({
+          type: "modal",
+          title: "Guard System",
+          content: "Blocked",
+          button1: "OK",
+          button2: "Sorry"
+        })
+      ).catch(() => {});
+      return false;
+    }
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,12 @@
-import {
-  SELECT_GUARD,
-  db
-} from "./database";
-
 import { system, MySystem } from "./system";
 
-declare function resolve(name: string): { register(input: IStoneServerSystem<MySystem> & MySystem): void }
+declare function resolve(
+  name: string
+): { register(input: IStoneServerSystem<MySystem> & MySystem): void };
 
 system.initialize = function() {
   this.registerRegionNames();
   resolve("commands/query-region").register(this);
   resolve("commands/create-region").register(this);
-  this.checkAbility(player => {
-    const info = this.actorInfo(player);
-    if (info.permission > 0) return true;
-    const ret = db.query(SELECT_GUARD, {
-      $x: info.pos[0],
-      $z: info.pos[2]
-    });
-    const filtered = Array.from(ret).filter(({ owner }) => owner != info.uuid);
-    if (filtered.length) {
-      this.openModalForm(
-        player,
-        JSON.stringify({
-          type: "modal",
-          title: "Guard System",
-          content: "Blocked",
-          button1: "OK",
-          button2: "Sorry"
-        })
-      ).catch(() => {});
-      return false;
-    }
-  });
+  resolve("actions/check-ability").register(this);
 };
